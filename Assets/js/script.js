2264d820b4e622ef90685bdcf5a70405d9983bba
kali19 <khadijah.syed.ali@gmail.com>
2023-04-13 15:55:49 -0400

    current day

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,5 +21,5 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
-var today = dayjs();
-$("#currentDay").text(today);
+var todayDisplay = dayjs().format("dddd, MMMM D");
+$("#currentDay").text(todayDisplay);
