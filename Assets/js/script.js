6741b984ded17186f872b388ec00f439940f1ac4
kali19 <khadijah.syed.ali@gmail.com>
2023-04-13 22:48:37 -0400

    comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,43 +1,18 @@
 
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-
-// document.getElementById("hour-9").addEventListener("click", userInput);
-// function userInput(){
-//   console.log("In userInput: "+this);
-//   localStorage.setItem("9 am", document.getElementById("hour-9-text").value); 
-// }
-
-// document.getElementsByClassName("saveBtn").addEventListener("click", saveInput);
-// function saveInput(){
-//   console.log("In saveInput: "+this.id);
-//   localStorage.setItem(this.id, document.getElementById(this.id).value); 
-// }
-
 var saveButtons = document.getElementsByClassName('saveBtn');
 
 
-
+//this is for the save button so when the user clicks on it then their input is saved to the local storage.
 for (var i = 0; i < saveButtons.length; i++) {
   (function (savedBtnIndex) {
     saveButtons[savedBtnIndex].addEventListener("click", function () {
       console.log("savedBtnIndex: " + savedBtnIndex);
-      //localStorage.setItem(this.parentElement.querySelector('.hour').innerText,
-      //  this.parentElement.querySelector('.description').value);
+      
 
       var newScheduleItem = {
         text: this.parentElement.querySelector('.description').value,
         index: savedBtnIndex,
       };
-
-      // newScheduleItem.scheduleText = ;
-      // newScheduleItem.scheduleIndex = ;
-      //scheduleItems.push(this.parentElement.querySelector('.description').value);
 
       scheduleItems.push(newScheduleItem);
       saveScheduleItemsToStorage(scheduleItems);
@@ -45,21 +20,9 @@ for (var i = 0; i < saveButtons.length; i++) {
   })(i);
 }
 
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
 
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-
-
-// add project to local storage
+//this code reads and saves the user inputed items from the local storage.
 var scheduleItems = readScheduleItemsFromStorage();
-
-
 
 function readScheduleItemsFromStorage() {
   var scheduleItems = localStorage.getItem('scheduleItems');
@@ -75,16 +38,14 @@ function readScheduleItemsFromStorage() {
 function saveScheduleItemsToStorage(scheduleItems) {
   localStorage.setItem('scheduleItems', JSON.stringify(scheduleItems));
 }
-//window.addEventListener("load", (event) => {
 
-//document.querySelectorAll('textarea').forEach((elem) => elem.value = "Desiered value");
 
 loadSchedule();
-
+//I used a nested for loop here in order to have the items load and display
 function loadSchedule() {
   var items = readScheduleItemsFromStorage();
   console.log(items);
-  //var descriptionList = document.getElementsByClassName('description');
+  
   var descriptionList = document.querySelectorAll('.description');
   for (var i = 0; i < descriptionList.length; i++) {
     for (var j = 0; j < items.length; j++) {
@@ -93,38 +54,17 @@ function loadSchedule() {
       }
     }
   }
-
-  // for (var i = 0; i < descriptionList.length; i++) {
-  //   (function (descriptionIndex) {
-  //     descriptionList[descriptionIndex].addEventListener("load", function () {
-  //       console.log("descriptionIndex: " + i);
-  //       localStorage.getItem(this.parentElement.querySelector('.hour').innerText,
-  //         this.parentElement.querySelector('.description').value);
-  //     }
-  // )})}
 }
 
 
-// $('#userInput').append("<br>");
-// for (var i = 0; i < localStorage.length; i++){
-//     $('#userInput').append(localStorage.key(i));
-//     $('#userInput').append(":");
-//     $('#userInput').append(localStorage.getItem(localStorage.key(i)));
-//     $('#userInput').append("<br>");
-
-// }
-
-
-
-
-//displays date
+//this displays the current date and time
 var todayDisplay = dayjs().format("dddd, MMMM D");
 $("#currentDay").text(todayDisplay);
 
 var currentTime = dayjs().format("H");
 
 var hourBlocks = document.querySelectorAll('.hour');
-
+//this sets the class for the blocks
 for (var i = 0; i<hourBlocks.length; i++) {
   if (hourBlocks[i].id < currentTime) {
     hourBlocks[i].parentElement.setAttribute('class','past');
